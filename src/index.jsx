174017cf66debec4238b993d2d1dc3639c0c8246
Daniela Kůsová <danielka.kusova@gmail.com>
2020-11-21 10:32:58 +0100

    Nefunguje vybíjí se notas

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ProgressPlugin } from 'webpack';
 import './index.html';
+import Joke from './Joke';
 
-render(
-  <>
-    <header>
-      <h1>React Starter</h1>
-    </header>
-    <main>
-      <p>Moje první React stránka</p>
-    </main>
-    <footer>Martin Podloucký</footer>
-  </>,
-  document.querySelector('#app'),
-);
+const App = () => {
+  return (
+    <>
+    {jokes.map((joke)=> (
+      <Joke
+        userId={joke.id}
+        userName={joke.name}
+        text={joke.text}
+        likes={joke.likes}
+        dislikes= {joke.dislikes}
+      />
+    </>
+  );
+};
+
+render(<App />, document.getElementById('app'));
