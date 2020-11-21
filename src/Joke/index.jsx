894c7633f8ce270d1491d7ea04d3f9617540c69b
Daniela Kůsová <danielka.kusova@gmail.com>
2020-11-21 13:05:04 +0100

    Přidaná hodnota likes a dislikes do useState a vytvořeny lajky a dislajky

--- src/Joke/index.jsx
+++ src/Joke/index.jsx
@@ -2,35 +2,38 @@ import React, { useState } from 'react';
 import './style.css';
 
 const Joke = (props) => {
-  const [liked, setLiked] = useState('');
-  const [disliked, setDisliked] = useState('');
+  const [liked, setLiked] = useState(props.likes);
+  const [disliked, setDisliked] = useState(props.dislikes);
+
+  const lajky = props.likes + liked;
+  const dislajky = props.dislikes + disliked;
 
   return (
-    <div class="joke">
-      <div class="joke__body">
-        <div class="joke__user">
-          <img class="user-avatar" src={`assets/img/${props.userId}.png`}/>
-          <p class="user-name">{props.userName}</p>
+    <div className="joke">
+      <div className="joke__body">
+        <div className="joke__user">
+          <img className="user-avatar" src={`assets/img/${props.userId}.png`} />
+          <p className="user-name">{props.userName}</p>
         </div>
 
-        <p class="joke__text">{props.text}</p>
+        <p className="joke__text">{props.text}</p>
       </div>
-      <div class="joke__likes">
+      <div className="joke__likes">
         <button
           onClick={() => setLiked(liked + 1)}
           id="btn-up"
-          class="btn-like btn-like--up"
+          className="btn-like btn-like--up"
         ></button>
-        <span id="likes-up" class="likes-count likes-count--up">
-          {props.likes}
+        <span id="likes-up" className="likes-count likes-count--up">
+          {lajky}
         </span>
         <button
           onClick={() => setDisliked(disliked + 1)}
           id="btn-down"
-          class="btn-like btn-like--down"
+          className="btn-like btn-like--down"
         ></button>
-        <span id="likes-down" class="likes-count likes-count--down">
-          {props.dislikes}
+        <span id="likes-down" className="likes-count likes-count--down">
+          {dislajky}
         </span>
       </div>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { ProgressPlugin } from 'webpack';
 import './index.html';
 import Joke from './Joke';
 import { jokes } from './jokes.js';
-
-
+import './style.css';
 
 const App = () => {
   return (
